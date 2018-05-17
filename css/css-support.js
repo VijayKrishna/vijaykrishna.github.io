cea6b70cb2dca0dd81195ccdbe0cdc4a3789918c
Vijay Krishna Palepu <vpalepu@uci.edu>
2018-05-17 02:16:17 -0700

    Sticky header

---
+++ css/css-support.js
@@ -0,0 +1,13 @@
+header = document.getElementById("header");
+
+var myScrollFunc = function() {
+  var y = window.scrollY;
+
+  if (y >= 40) {
+    header.style.display = "block";
+  } else {
+    header.style.display = "none";
+  }
+};
+
+window.addEventListener("scroll", myScrollFunc);
