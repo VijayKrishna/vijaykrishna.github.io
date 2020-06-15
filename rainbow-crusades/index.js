cc870b86dc2ea68f1f437bfab412a154991fbf8c
Vijay Krishna Palepu <vpalepu@uci.edu>
2020-06-15 03:04:18 -0700

    rbc: calming scroll, and colors

--- rainbow-crusades/index.js
+++ rainbow-crusades/index.js
@@ -1,4 +1,3 @@
-
 let mainContainer = d3.select("div#main")
 console.log(mainContainer)
 
@@ -30,13 +29,14 @@ let contentDivs = mainContainer.selectAll("div")
                             .enter()
                             .append("div")
                             .attr("class", "w-auto p-2 mx-auto my-5 shadow rounded-lg")
+                            .style("background-color", "white")
                             .attr("id", (d, i) => "rbc-spy" + i)
                             .on("click", function() {
                                 focusIntoView(this, true)
                             })
 
 contentDivs.append("img")
-        .attr("class", "w-50 d-flex mx-auto rounded-lg")
+        .attr("class", "w-50 d-inline-block rounded-lg")
         .attr("src", d => `https://www.instagram.com/p/${d.ref}/media/?size=l`)
         .attr("crossorigin", "anonymous")
         .on("load", function(d) {
@@ -45,39 +45,75 @@ contentDivs.append("img")
         })
 
 contentDivs.append("p")
-            .attr("class", "w-50 d-flex mx-auto mt-2 mb-0 rounded-lg px-2")
+            .attr("class", "w-50 d-inline-block rounded-lg px-2")
             .style("color", d => d.fgCol)
             .html((d, i) => "Caption: " + d.caption)
 
-function focusIntoView(target, placeInCenter = false) {
-        //ref: https://hiddedevries.nl/en/blog/2018-12-10-scroll-an-element-into-the-center-of-the-viewport
-        // {
-        //     behavior: "smooth" | "auto";
-        //     block: "start" | "center" | "end" | "nearest";
-        //     inline: "start" | "center" | "end" | "nearest";
-        // }
-        
+function modulatedBgColor(bgCol) {
+    const col = d3.color(bgCol)
+
+    const hexCol = col.hex()
+
+    // make exceptions for select colors
+    const exceptionHexs = ["#000000", "#ffffff"]
+    const exceptionLookup = exceptionHexs.indexOf(hexCol)
+    if (exceptionLookup >= 0 && exceptionLookup < exceptionHexs.length) {
+        return hexCol
+    }
+
+    // refs
+    // 1. working with d3.color: https://observablehq.com/@d3/working-with-color
+    // 2. d3.color api: https://github.com/d3/d3-color
+    const lch = d3.lch(col)
+
+    // How I arrived at these L and C values:
+    // 1. Played around with an HCL color picker: https://bl.ocks.org/mbostock/3e115519a1b495e0bd95
+    // 2. I wanted a subtle/subdued color, which was not too light, and did not give off too much color.
+    //    Reducing the Chroma to near zero seemed to do the trick here, while mainting luminance somewhere in the middle
+    const subtleCol = d3.lch(90, 5, lch.h)
+    return subtleCol.hex()
+}
+
+d3.select("body").style("background-color", "#ffffff")
+
+function focusIntoView(target, positionInViewportCenter = false) {
+    //ref: https://hiddedevries.nl/en/blog/2018-12-10-scroll-an-element-into-the-center-of-the-viewport
+    // {
+    //     behavior: "smooth" | "auto";
+    //     block: "start" | "center" | "end" | "nearest";
+    //     inline: "start" | "center" | "end" | "nearest";
+    // }
+    
+    if (positionInViewportCenter) {
         target.scrollIntoView({
             behavior: "smooth",
-            block: placeInCenter ? "center" : "nearest"
+            block: "center"
         })
-        let targetSel = d3.select(target)
-        let bgCol = targetSel.style("background-color")
-        const col = d3.color(bgCol)
-        d3.select("body")
-            .transition()
-            .style("background-color", col.brighter(0.5))
+    }
+
+    let targetSel = d3.select(target)
+    let bgCol = targetSel.style("background-color")
+
+    const modBgCol = modulatedBgColor(bgCol)
+
+    d3.select("body")
+        .transition()
+        .style("background-color", modBgCol)
 }
+
+// 
 
 var observer = new IntersectionObserver(function(entries) {
     // isIntersecting is true when element and viewport are overlapping
     // isIntersecting is false when element and viewport don't overlap
 
+    console.log(entries.length)
+
     let firstEntry = entries[0]
 
     if(firstEntry.isIntersecting === true) {
         let target = firstEntry.target
-        focusIntoView(target, true)
+        focusIntoView(target, false)
     }
 
 }, { threshold: [0.75] });
